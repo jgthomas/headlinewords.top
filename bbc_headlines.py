0304759550ef89369c258c02377cf8637d68d8d2
James Thomas <dairy_water@yahoo.co.uk>
2017-04-29 10:29:23 +0100

    docstrings in bbc_headlines

--- bbc_headlines.py
+++ bbc_headlines.py
@@ -30,36 +30,50 @@ TOP_WORDS = 10
 
 
 def no_numbers(x):
+    """ Remove words beginning with a digit. """
     return re.sub(r'^[0-9].+$', '', x)
 
 def no_non_word(x):
+    """ Remove non-word characters. """
     return re.sub(r'\W', '', x)
 
 def too_short(x):
+    """ Return True if word is too short. """
     return len(x) > SHORT_WORD
 
 def lower(x):
+    """ Return x in lowercase. """
     return x.lower()
 
 
 def get_words(url):
+    """
+    Pulls down the RSS feed from BBC News and returns 
+    all the words found in the headlines.
+
+    """
     feed = feedparser.parse(url)
     headlines = (x['title'].split() for x in feed['entries'])
     headline_words = (word for headline in headlines for word in headline)
     return headline_words
 
 
-# Maps
+# Maps - apply function to every word
 to_lowercase = map_over(lower)
 del_non_word_chars = map_over(no_non_word)
 
-# Filters
+# Filters - remove words based on these
 remove_function_words = filter_by(is_not_in(IGNORE))
 remove_numbers = filter_by(no_numbers)
 remove_short_words = filter_by(too_short)
 
 
 def filter_words(words):
+    """
+    Transforms the words through application of the 
+    maps and filters to every element.
+
+    """
     filtered_words = (del_non_word_chars
                       (remove_short_words
                        (remove_numbers
@@ -69,6 +83,7 @@ def filter_words(words):
 
 
 def save_words(words):
+    """ Save the words to a JSON file. """
     with open(TODAYS_WORDS, 'w') as outfile:
         json.dump(words, outfile)
 
