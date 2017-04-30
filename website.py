3e6423dc2c124fdf4022eb03d3abafebd77b9ad6
James Thomas <dairy_water@yahoo.co.uk>
2017-04-30 13:48:20 +0100

    weekly and monthly now live

--- website.py
+++ website.py
@@ -6,15 +6,21 @@ import json
 from flask import Flask, render_template
 
 from funcs import load_words
-from constants import TODAYS_WORDS, TOP_WORDS
+from constants import (TODAYS_WORDS, 
+                       TOP_WORDS,
+                       WEEK,
+                       MONTH)
 
-from query_database import query, overall_total
-
+from query_database import (query, 
+                            overall_total,
+                            since_date)
 
 app = Flask(__name__)
 
 
 WORDS = load_words(TODAYS_WORDS)[:TOP_WORDS]
+WEEK_WORDS = query(since_date, (WEEK,))[:TOP_WORDS]
+MONTH_WORDS = query(since_date, (MONTH,))[:TOP_WORDS]
 ALL_TIME_WORDS = query(overall_total)[:TOP_WORDS]
 ALL_WORDS = query(overall_total)
 
@@ -22,6 +28,16 @@ ALL_WORDS = query(overall_total)
 @app.route('/')
 def display_words(words=WORDS):
     return render_template('words_now.html', words=words)
+
+
+@app.route('/week')
+def show_week_words(words=WEEK_WORDS):
+    return render_template('words_week.html', words=WEEK_WORDS)
+
+
+@app.route('/month')
+def show_month_words(words=MONTH_WORDS):
+    return render_template('words_month.html', words=MONTH_WORDS)
 
 
 @app.route('/alltime')
