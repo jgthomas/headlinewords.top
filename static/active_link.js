9d68a1433c49980f39e40e5a300344c0af5ab17c
James Thomas <dairy_water@yahoo.co.uk>
2017-05-02 21:38:11 +0100

    script

--- static/active_link.js
+++ static/active_link.js
@@ -1,6 +1,12 @@
-$(document).ready(function() {
-   var links = $('a.link').click(function() {
-       links.removeClass('current');
-       $(this).addClass('current');
-   });
+document.addEventListener("DOMContentLoaded", function() {
+        var x = document.getElementsByClassName('navlink')
+        for (var i = 0; i < x.length; i++) {
+                x[i].addEventListener("click", function(){
+                        var selectedEl = document.querySelector(".current");
+                        if(selectedEl){
+                                selectedEl.classList.remove("current");
+                        }
+                        this.classList.add("current");
+                }, false);;
+        }
 });
