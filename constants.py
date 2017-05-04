e7a46dbe90e7984c213e80dff33d395a77b056e2
James Thomas <F0llydog@protonmail.com>
2017-05-04 12:42:52 +0100

    added some more words to ignore

--- constants.py
+++ constants.py
@@ -20,8 +20,9 @@ TODAYS_WORDS = '_'.join([DATE, NAME])
 
 IGNORE = ['and', 'a', 'if', 'on', 'no', 'yes', 'by', 'for', 'to', 'in',
           'is', 'are', 'be', 'or', 'as', 'where', 'when', 'how', 'the',
-          'off', 'say', 'with', 'what', 'our', 'can', 'not', 'say',
-          'says', 'over', 'just', 'that', 'do', 'does', 'from']
+          'off', 'say', 'with', 'what', 'our', 'can', 'not', 'an',
+          'says', 'over', 'just', 'that', 'do', 'does', 'from', 'too',
+          'why', 'out', 'mr', 'ms', 'miss', 'mrs', 'dr', 'it']
 
 SHORT_WORD = 2
 
