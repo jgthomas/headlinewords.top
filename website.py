7c8495c67d257baeafc45fbf67376497faa14210
James Thomas <dairy_water@yahoo.co.uk>
2017-05-04 07:06:23 +0100

    simplified template naming and links

--- website.py
+++ website.py
@@ -21,30 +21,30 @@ app = Flask(__name__)
 WORDS = load_words(TODAYS_WORDS)[:TOP_WORDS]
 WEEK_WORDS = query(since_date, (WEEK,))[:TOP_WORDS]
 MONTH_WORDS = query(since_date, (MONTH,))[:TOP_WORDS]
-ALL_TIME_WORDS = query(overall_total)[:TOP_WORDS]
+EVER_WORDS = query(overall_total)[:TOP_WORDS]
 ALL_WORDS = query(overall_total)
 
 
 @app.route('/')
-def display_words(words=WORDS):
-    return render_template('words_now.html', words=words)
+def now(words=WORDS):
+    return render_template('now.html', words=words)
 
 
 @app.route('/week')
-def show_week_words(words=WEEK_WORDS):
-    return render_template('words_week.html', words=WEEK_WORDS)
+def week(words=WEEK_WORDS):
+    return render_template('week.html', words=WEEK_WORDS)
 
 
 @app.route('/month')
-def show_month_words(words=MONTH_WORDS):
-    return render_template('words_month.html', words=MONTH_WORDS)
+def month(words=MONTH_WORDS):
+    return render_template('month.html', words=MONTH_WORDS)
 
 
-@app.route('/alltime')
-def show_all_time_words(words=ALL_TIME_WORDS):
-    return render_template('words_alltime.html', words=ALL_TIME_WORDS)
+@app.route('/ever')
+def ever(words=EVER_WORDS):
+    return render_template('ever.html', words=EVER_WORDS)
 
 
 @app.route('/all')
-def show_all_words(words=ALL_WORDS):
-    return render_template('words_all.html', words=ALL_WORDS)
+def all(words=ALL_WORDS):
+    return render_template('all.html', words=ALL_WORDS)
