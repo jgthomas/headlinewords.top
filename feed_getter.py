24428b2d96458ecbfb7962272a023648e4b0dec4
James Thomas <dairy_water@yahoo.co.uk>
2017-05-14 07:28:11 +0100

    switched on new york times backend

--- feed_getter.py
+++ feed_getter.py
@@ -6,6 +6,7 @@ import feedparser
 
 BBC_URL = 'http://feeds.bbci.co.uk/news/uk/rss.xml'
 NYT_URL = 'http://rss.nytimes.com/services/xml/rss/nyt/US.xml'
+GDN_URL = 'https://www.theguardian.com/uk-news/rss'
 
 
 def get_headlines(url):
@@ -18,6 +19,7 @@ def get_headlines(url):
 # Sources
 BBC = get_headlines(BBC_URL)
 NYT = get_headlines(NYT_URL)
+GDN = get_headlines(GDN_URL)
 
 # Currently used sources
-SOURCES = [('bbc', BBC)]
+SOURCES = [('bbc', BBC), ('nyt', NYT)]
