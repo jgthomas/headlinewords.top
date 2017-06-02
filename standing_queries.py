de3958de4cf32e7c9196a4c8ca42b2f670d1a0ff
James Thomas <dairy_water@yahoo.co.uk>
2017-06-02 07:58:20 +0100

    much more generic templating

--- standing_queries.py
+++ standing_queries.py
@@ -5,12 +5,14 @@ from query import data
 from trending_words import trends
 
 
-## BASE DATA
+## BASE DATA ##
 BBC_BASE = {"source": "bbc", "title": "BBC"}
 NYT_BASE = {"source": "nyt", "title": "New York Times"}
+DML_BASE = {"source": "dml", "title": "Daily Mail"}
+FOX_BASE = {"source": "fox", "title": "Fox News"}
 
 
-## TIME PERIODS
+## TIME PERIODS ##
 BBC_TODAY = {**BBC_BASE, **{"data": data("bbc", "ondate", "today")}}
 BBC_WEEK = {**BBC_BASE, **{"data": data("bbc", "since", "week")}}
 BBC_MONTH = {**BBC_BASE, **{"data": data("bbc", "since", "month")}}
@@ -21,28 +23,46 @@ NYT_WEEK = {**NYT_BASE, **{"data": data("nyt", "since", "week")}}
 NYT_MONTH = {**NYT_BASE, **{"data": data("nyt", "since", "month")}}
 NYT_EVER = {**NYT_BASE, **{"data": data("nyt", "ever")}}
 
+DML_TODAY = {**DML_BASE, **{"data": data("dml", "ondate", "today")}}
+DML_WEEK = {**DML_BASE, **{"data": data("dml", "since", "week")}}
+DML_MONTH = {**DML_BASE, **{"data": data("dml", "since", "month")}}
+DML_EVER = {**DML_BASE, **{"data": data("dml", "ever")}}
+
+FOX_TODAY = {**FOX_BASE, **{"data": data("fox", "ondate", "today")}}
+FOX_WEEK = {**FOX_BASE, **{"data": data("fox", "since", "week")}}
+FOX_MONTH = {**FOX_BASE, **{"data": data("fox", "since", "month")}}
+FOX_EVER = {**FOX_BASE, **{"data": data("fox", "ever")}}
+
+
+## MONTHS
+# Base
+MAY_BASE = {"title": "May"}
+JUN_BASE = {"title": "June"}
+# Sources
+BBC_MAY = {**MAY_BASE, **{"data": data("bbc", "between", "may_start", "jun_start")}}
+BBC_JUN = {**JUN_BASE, **{"data": data("bbc", "between", "jun_start", "jul_start")}}
+
+NYT_MAY = {**MAY_BASE, **{"data": data("nyt", "between", "may_start", "jun_start")}}
+NYT_JUN = {**JUN_BASE, **{"data": data("nyt", "between", "jun_start", "jul_start")}}
+
 
 ## TRENDING
-BBC_TODAY_RISING = {"title": "Rising"}
-BBC_TODAY_FALLING = {"title": "Falling"}
+# Base
+RISING_BASE = {"title": "Rising"}
+FALLING_BASE = {"title": "Falling"}
+# Sources
+BBC_TODAY_RISING = {**RISING_BASE}
+BBC_TODAY_FALLING  = {**FALLING_BASE}
 BBC_TODAY_RISING["data"], BBC_TODAY_FALLING["data"] = trends("bbc", "day_on_day")
 
-BBC_WEEK_RISING = {"title": "Rising"}
-BBC_WEEK_FALLING = {"title": "Falling"}
+BBC_WEEK_RISING = {**RISING_BASE}
+BBC_WEEK_FALLING  = {**FALLING_BASE}
 BBC_WEEK_RISING["data"], BBC_WEEK_FALLING["data"] = trends("bbc", "week_on_week")
 
-#BBC_MONTH_RISING = {"title": "Rising"}
-#BBC_MONTH_FALLING = {"title": "Falling"}
-#BBC_MONTH_RISING["data"], BBC_MONTH_FALLING["data"] = trends("bbc", "month_on_month")
-
-NYT_TODAY_RISING = {"title": "Rising"}
-NYT_TODAY_FALLING = {"title": "Falling"}
+NYT_TODAY_RISING = {**RISING_BASE}
+NYT_TODAY_FALLING  = {**FALLING_BASE}
 NYT_TODAY_RISING["data"], NYT_TODAY_FALLING["data"] = trends("nyt", "day_on_day")
 
-NYT_WEEK_RISING = {"title": "Rising"}
-NYT_WEEK_FALLING = {"title": "Falling"}
+NYT_WEEK_RISING = {**RISING_BASE}
+NYT_WEEK_FALLING  = {**FALLING_BASE}
 NYT_WEEK_RISING["data"], NYT_WEEK_FALLING["data"] = trends("nyt", "week_on_week")
-
-#NYT_MONTH_RISING = {"title": "Rising"}
-#NYT_MONTH_FALLING = {"title": "Falling"}
-#NYT_MONTH_RISING["data"], NYT_MONTH_FALLING["data"] = trends("nyt", "month_on_month")
--- website.py
+++ website.py
@@ -10,20 +10,9 @@ from query_functions import just_words
 
 from query import data
 
-from standing_queries import (BBC_TODAY, BBC_WEEK, BBC_MONTH, BBC_EVER,
-                              NYT_TODAY, NYT_WEEK, NYT_MONTH, NYT_EVER,
-                              BBC_TODAY_RISING, BBC_TODAY_FALLING,
-                              BBC_WEEK_RISING, BBC_WEEK_FALLING,
-                              NYT_TODAY_RISING, NYT_TODAY_FALLING,
-                              NYT_WEEK_RISING, NYT_WEEK_FALLING)
+from standing_queries import *
 
-from graph_configs import (HOMEPAGE_BBC_1, HOMEPAGE_NYT_1,
-                           BBC_TODAY_1, BBC_TODAY_2,
-                           BBC_WEEK_1, BBC_WEEK_2,
-                           BBC_MONTH_1, BBC_MONTH_2,
-                           BBC_EVER_1, BBC_EVER_2,
-                           NYT_WEEK_1,
-                           NYT_MONTH_1, NYT_MONTH_2)
+from graph_configs import *
 
 
 app = Flask(__name__)
@@ -54,21 +43,17 @@ def read_top_nyt_words():
 ### HOMEPAGE ###
 @app.route('/')
 def home():
-    NEW_SOURCE_1 = {"title": "Daily Mail",
-                    "data": data('dml', 'ondate', 'today')}
-    NEW_SOURCE_2 = {"title": "Fox News",
-                    "data": data('fox', 'ondate', 'today')}
     return render_template('index.html',
                            title = 'BBC - Top words today',
                            sources = (BBC_TODAY, NYT_TODAY,
-                                      NEW_SOURCE_1, NEW_SOURCE_2),
+                                      DML_TODAY, FOX_TODAY),
                            display = SHORT_N_WORDS,
                            plots = (HOMEPAGE_BBC_1, HOMEPAGE_NYT_1))
 
 ### BBC PAGES ###
 @app.route('/bbc/today')
 def bbc_today():
-    return render_template('bbc.html',
+    return render_template('source.html',
                            title = 'BBC - Top words today',
                            source = BBC_TODAY,
                            display = TOP_N_WORDS,
@@ -78,7 +63,7 @@ def bbc_today():
 
 @app.route('/bbc/week')
 def bbc_week():
-    return render_template('bbc.html',
+    return render_template('source.html',
                            title = 'BBC - Top words this week',
                            source = BBC_WEEK,
                            display = TOP_N_WORDS,
@@ -88,7 +73,7 @@ def bbc_week():
 
 @app.route('/bbc/month')
 def bbc_month():
-    return render_template('bbc.html',
+    return render_template('source.html',
                            title = 'BBC - Top words this month',
                            source = BBC_MONTH,
                            display = TOP_N_WORDS,
@@ -96,15 +81,11 @@ def bbc_month():
 
 @app.route('/bbc/ever')
 def bbc_ever():
-    MAY = {"title": "May",
-           "data": data("bbc", "between", "may_start", "jun_start")}
-    JUNE = {"title": "June",
-            "data": data("bbc", "between", "jun_start", "jul_start")}
-    return render_template('bbc.html',
+    return render_template('source.html',
                            title = 'BBC - Top words ever',
                            source = BBC_EVER,
                            display = DOUBLE_N_WORDS,
-                           extras = (MAY, JUNE),
+                           extras = (BBC_MAY, BBC_JUN),
                            extra_display = TOP_N_WORDS,
                            plots = (BBC_EVER_1, BBC_EVER_2))
 
@@ -112,7 +93,7 @@ def bbc_ever():
 ### NYT PAGES ###
 @app.route('/nyt/today')
 def nyt_today():
-    return render_template('nyt.html',
+    return render_template('source.html',
                            title = 'New York Times - Top words today',
                            source = NYT_TODAY,
                            display = TOP_N_WORDS,
@@ -121,7 +102,7 @@ def nyt_today():
 
 @app.route('/nyt/week')
 def nyt_week():
-    return render_template('nyt.html',
+    return render_template('source.html',
                            title = 'New York Times - Top words this week',
                            source = NYT_WEEK,
                            display = TOP_N_WORDS,
@@ -131,7 +112,7 @@ def nyt_week():
 
 @app.route('/nyt/month')
 def nyt_month():
-    return render_template('nyt.html',
+    return render_template('source.html',
                            title = 'New York Times - Top words this month',
                            source = NYT_MONTH,
                            display = TOP_N_WORDS,
@@ -139,16 +120,65 @@ def nyt_month():
 
 @app.route('/nyt/ever')
 def nyt_ever():
-    MAY = {"title": "May",
-           "data": data("nyt", "between", "may_start", "jun_start")}
-    JUNE = {"title": "June",
-            "data": data("nyt", "between", "jun_start", "jul_start")}
-    return render_template('nyt.html',
+    return render_template('source.html',
                            title = 'New York Times - Top words ever',
                            source = NYT_EVER,
                            display = DOUBLE_N_WORDS,
-                           extras = (MAY, JUNE),
+                           extras = (NYT_MAY, NYT_JUN),
                            extra_display = TOP_N_WORDS)
+
+### DAILY MAIL ###
+@app.route('/dml/today')
+def dml_today():
+    return render_template('source.html',
+                           title = 'Daily Mail - Top words today',
+                           source = DML_TODAY,
+                           display = TOP_N_WORDS)
+@app.route('/dml/week')
+def dml_week():
+    return render_template('source.html',
+                           title = 'Daily Mail - Top words this week',
+                           source = DML_WEEK,
+                           display = TOP_N_WORDS)
+@app.route('/dml/month')
+def dml_month():
+    return render_template('source.html',
+                           title = 'Daily Mail - Top words this month',
+                           source = DML_MONTH,
+                           display = TOP_N_WORDS)
+@app.route('/dml/ever')
+def dml_ever():
+    return render_template('source.html',
+                           title = 'Daily Mail - Top words ever',
+                           source = DML_EVER,
+                           display = DOUBLE_N_WORDS)
+
+
+### FOX NEWS ###
+@app.route('/fox/today')
+def fox_today():
+    return render_template('source.html',
+                           title = 'Fox News - Top words today',
+                           source = FOX_TODAY,
+                           display = TOP_N_WORDS)
+@app.route('/fox/week')
+def fox_week():
+    return render_template('source.html',
+                           title = 'Fox News - Top words this week',
+                           source = FOX_WEEK,
+                           display = TOP_N_WORDS)
+@app.route('/fox/month')
+def fox_month():
+    return render_template('source.html',
+                           title = 'Fox News - Top words this month',
+                           source = FOX_MONTH,
+                           display = TOP_N_WORDS)
+@app.route('/fox/ever')
+def fox_ever():
+    return render_template('source.html',
+                           title = 'Fox News - Top words ever',
+                           source = FOX_EVER,
+                           display = DOUBLE_N_WORDS)
 
 
 ### COMBINED PAGES ###
