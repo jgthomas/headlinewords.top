1faa8388d27277b302fa984d1e9668417879fe33
James Thomas <dairy_water@yahoo.co.uk>
2017-06-03 18:58:43 +0100

    moved more graphs to week cadence

--- graph_configs.py
+++ graph_configs.py
@@ -20,16 +20,18 @@ HOMEPAGE_BBC_1 = Graph(db = BBC_DB,
 
 HOMEPAGE_NYT_1 = Graph(db = NYT_DB,
                        filename = "homepage_nyt_1",
-                       days = "14",
-                       words = just_words(data("nyt", "ever"))[:SHORT_N_WORDS])
+                       days = "7",
+                       words = just_words(data("nyt", "ever"))[:SHORT_N_WORDS],
+                       period = "4")
 
 
 ### BBC TODAY ###
 BBC_TODAY_1 = Graph(db = BBC_DB,
                     filename = "bbc_today_1",
-                    days = "14",
+                    days = "7",
                     words = ['conservative', 'labour'],
-                    colour = ['blue', 'red'])
+                    colour = ['blue', 'red'],
+                    period = "4")
 
 BBC_TODAY_2 = Graph(db = BBC_DB,
                     filename = "bbc_today_2",
@@ -66,8 +68,9 @@ BBC_MONTH_2 = Graph(db = BBC_DB,
 ### BBC EVER ###
 BBC_EVER_1 = Graph(db = BBC_DB,
                    filename = "bbc_ever_1",
-                   days = "30",
-                   words = just_words(data("bbc", "ever"))[:SHORT_N_WORDS])
+                   days = "7",
+                   words = just_words(data("bbc", "ever"))[:SHORT_N_WORDS],
+                   period = "4")
 
 BBC_EVER_2 = Graph(db = BBC_DB,
                    filename = "bbc_ever_2",
@@ -88,13 +91,15 @@ NYT_WEEK_1 = Graph(db = NYT_DB,
 ### NYT MONTH ###
 NYT_MONTH_1 = Graph(db = NYT_DB,
                     filename = 'nyt_month_1',
-                    days = "30",
-                    words = ['trump', 'ivanka', 'kushner', 'melania'])
+                    days = "7",
+                    words = ['trump', 'ivanka', 'kushner', 'melania'],
+                    period = "4")
 
 NYT_MONTH_2 = Graph(db = NYT_DB,
                     filename = 'nyt_month_2',
-                    days = "30",
-                    words = ['fbi', 'comey', 'russia', 'investigation'])
+                    days = "7",
+                    words = ['fbi', 'comey', 'russia', 'investigation'],
+                    period =  "4")
 
 ### NYT EVER ###
 
