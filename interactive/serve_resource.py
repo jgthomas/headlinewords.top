3c151755b5c3991a6860d2a2728944889cd9bafd
James Thomas <dairy_water@yahoo.co.uk>
2017-06-21 07:47:14 +0100

    more explicit name for graph form

--- interactive/serve_resource.py
+++ interactive/serve_resource.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 import plot_words
-from graph import Graph
+from graph import Graph, get_plot
 from constants import SOURCE_BASE
 
 
@@ -9,7 +9,7 @@ serve = Blueprint("serve", __name__)
 
 @app.route("/graph")
 def graph():
-    word = request.args.get("word", "")
+    word = request.args.get("word_to_graph", "")
     source = request.args.get("check", "")
 
     graph = Graph(db = source,
@@ -19,9 +19,9 @@ def graph():
                   colour = ["blue"],
                   period = "4")
 
-    plot_1 = get_plot(title=word,
-                      filename=''.join([word, ".png"])
+    plot = get_plot(title=word,
+                    filename=''.join([word, ".png"])
     source_details = SOURCE_BASE[source]
     return render_template("output.html",
                            publication=source_details,
-                           plot=(plot1,))
+                           plot=plot)
