e1c7a1aa9d9841bb23561305044dc1795cd6454c
James Thomas <F0llydog@protonmail.com>
2017-06-21 22:11:52 +0100

    blank graph search redirects back to search box

--- interactive/serve_resource.py
+++ interactive/serve_resource.py
@@ -1,5 +1,5 @@
 import os.path
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 import plot_words
 from graph import Graph, get_plot
 from constants import SOURCE_BASE, PLOT_PATH
@@ -9,27 +9,30 @@ from query_functions import TODAY
 serve = Blueprint("serve", __name__)
 
 
-@serve.route("/graph", methods=['GET'])
+@serve.route("/graph", methods=["GET"])
 def graph():
     word = request.args.get("word_to_graph", "").lower()
     source = request.args.get("check", "")
+    if not word:
+        return redirect(url_for("home", _anchor="pick-graph"))
+
     date = TODAY.strftime("_%d_%B_%Y")
-    filename = ''.join([word, '_', source, date, '.png'])
+    filename = ''.join([word, '_', source, date])
+    full_filename = ''.join([filename, ".png"])
 
     if not os.path.isfile(''.join(["./", PLOT_PATH, filename])):
         graph = Graph(db = source,
-                  filename = ''.join([word, '_', source, date]),
+                  filename = filename,
                   days = "7",
                   words = [word],
                   colour = ["blue"],
                   period = "4")
-
         plot_words.main(graph.args())
 
     source_details = SOURCE_BASE[source]
+
     plot = get_plot(title=word,
-                    filename=filename,
+                    filename=full_filename,
                     source=source_details["title"])
     return render_template("output.html",
-                           #publication=source_details,
                            plot=plot)
